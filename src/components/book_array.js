0f7fcbbf719f6d866b9d43e7b12bf81d9a829e51
Marc raphael <marcraphael979@gmail.com>
2023-01-20 10:22:12 +0100

    components/book_array: Add book type

--- src/components/book_array.js
+++ src/components/book_array.js
@@ -3,18 +3,21 @@ const BookList = [
     id: 1,
     title: 'The Hobbit',
     author: 'J.R.R. Tolkien',
+    type: 'Action',
   },
 
   {
     id: 2,
     title: 'Harry Potter and the Chamber of Secrets',
     author: 'J.K. Rowling',
+    type: 'Fantastics',
   },
 
   {
     id: 3,
     title: 'The Lord of the Rings',
     author: 'J.K. Rowling',
+    type: 'Fantastics',
   },
 ];
 
