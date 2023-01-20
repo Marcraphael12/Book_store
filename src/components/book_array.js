6cfd41fc2e7760452ec0293c77dd2b534fb3de23
Marc raphael <marcraphael979@gmail.com>
2023-01-20 10:13:34 +0100

    components/book_array: create Book list

---
+++ src/components/book_array.js
@@ -0,0 +1,21 @@
+const BookList = [
+  {
+    id: 1,
+    title: 'The Hobbit',
+    author: 'J.R.R. Tolkien',
+  },
+
+  {
+    id: 2,
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling',
+  },
+
+  {
+    id: 3,
+    title: 'The Lord of the Rings',
+    author: 'J.K. Rowling',
+  },
+];
+
+export default BookList;
