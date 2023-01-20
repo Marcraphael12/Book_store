03b35ccd840d0cc9e72362931016dc1c2cbd2c72
Marc raphael <marcraphael979@gmail.com>
2023-01-20 09:51:51 +0100

    App: Add routes

--- src/App.js
+++ src/App.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+
+import Header from './components/Header';
+
+const App = () => {
+  <>
+    <Header />
+    <Routes>
+      <Route path="/books" element={<Books />} />
+    </Routes>
+  </>;
+};
+
+export default App;
