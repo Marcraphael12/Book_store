1d0201368eb09569eb78557e555bea419782631d
Marc raphael <marcraphael979@gmail.com>
2023-01-24 03:11:11 +0100

    components/add_a_book/Add_a_Book: Add state and dipatch function

--- src/components/add_a_book/Add_a_Book.js
+++ src/components/add_a_book/Add_a_Book.js
@@ -4,3 +4,8 @@ import { useDispatch } from 'react-redux';
 import { uniqueId as uuidv4 } from 'uuid';
 
 import { addBook } from '../redux/books/books';
+
+const AddBook = () => {
+  const [title, setTitle] = useState('');
+  const dispatch = useDispatch();
+};
