9a9de2325b4fe5aff628a6220435ddf968950996
Marc raphael <marcraphael979@gmail.com>
2023-01-24 04:32:20 +0100

    components/remove_a_book/Remove_a_Book: Add remove book function

---
+++ src/components/remove_a_book/Remove_a_Book.js
@@ -0,0 +1,14 @@
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
+
+const RemoveBook = (id) => {
+  const dispatch = useDispatch();
+
+  const Remove = () => {
+    dispatch(removeBook(id));
+  };
+
+  return Remove();
+};
+
+export default RemoveBook;
