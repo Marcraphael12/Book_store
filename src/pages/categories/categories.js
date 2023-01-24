a3906cd9474fe1aac821164a5f45aa3718f6f7ac
Marc raphael <marcraphael979@gmail.com>
2023-01-24 04:33:32 +0100

    pages/categories/categories: Add categories page

---
+++ src/pages/categories/categories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function categories() {
+  return (
+    <div>
+      <button type="button">
+        Check Status
+      </button>
+    </div>
+  );
+}
+
+export default categories;
