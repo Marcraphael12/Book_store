afe96ce1cea23382d28f9fc14b34cd58fa7f2b4d
Marc raphael <marcraphael979@gmail.com>
2023-01-24 04:35:13 +0100

    index: Add provider and render content

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Provider } from 'react-redux';
+
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import store from './components/redux/configureStore';
+
+import App from './App';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+);
