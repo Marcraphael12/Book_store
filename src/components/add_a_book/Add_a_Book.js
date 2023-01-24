30d2d8dde3f2a455e3e3645ec35bdcaccfa3865c
Marc raphael <marcraphael979@gmail.com>
2023-01-24 03:39:05 +0100

    components/add_a_book/Add_a_Book: Add form for adding book

--- src/components/add_a_book/Add_a_Book.js
+++ src/components/add_a_book/Add_a_Book.js
@@ -7,5 +7,50 @@ import { addBook } from '../redux/books/books';
 
 const AddBook = () => {
   const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
   const dispatch = useDispatch();
+
+  // state handlers
+  const AddTitle = (e) => setTitle(e.target.value);
+  const AddAuthor = (e) => setAuthor(e.target.value);
+  const clearInputs = () => {
+    setTitle('');
+    setAuthor('');
+  };
+
+  const Submit = (e) => {
+    e.preventDefault();
+    const newBook = {
+      id: uuidv4(),
+      title,
+      author,
+    };
+
+    // dispatch action
+    dispatch(addBook(newBook));
+
+    // reset input
+    clearInputs();
+  };
+
+  return (
+    <form onSubmit={Submit}>
+      <input
+        type="text"
+        placeholder="Book title"
+        value={title}
+        onChange={AddTitle}
+      />
+
+      <input
+        type="text"
+        placeholder="Book's author"
+        value={author}
+        onChange={AddAuthor}
+      />
+      <button type="submit">Add book</button>
+    </form>
+  );
 };
+
+export default AddBook;
