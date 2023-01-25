313f9eba7992283352c4dfdf21ec7aced849b0de
Marc raphael <marcraphael979@gmail.com>
2023-01-25 22:24:11 +0100

    components/simple_book/Single_Book: update syntaxe

--- src/components/simple_book/Single_Book.js
+++ src/components/simple_book/Single_Book.js
@@ -1,29 +1,29 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 
-import useDispatch from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { removeBook } from '../../redux/books/books';
 
-const SingleBook = (props) => {
-  const { title, author, id } = props;
+const SingleBook = (book) => {
+  const { title, author, id } = book;
   const dispatch = useDispatch();
-  const RemoveBook = (id) => {
+  const RemoveBook = () => {
     dispatch(removeBook(id));
   };
   return (
-    <div className="book">
+    <li className="book">
       <div className="book-info">
         <h3 className="book-title">{title}</h3>
         <h4 className="book-author">{author}</h4>
       </div>
-      <button type="button" onClick={RemoveBook(id)}>Remove Book</button>
-    </div>
+      <button type="button" onClick={RemoveBook}>Remove Book</button>
+    </li>
   );
 };
 
 export default SingleBook;
 
 SingleBook.propTypes = {
-  id: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
+  book: PropTypes.shape({ id: PropTypes.string, title: PropTypes.string, author: PropTypes.string })
+    .isRequired,
 };
