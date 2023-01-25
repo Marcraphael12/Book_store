20743ee5a59870a2c39158385c59226068cd8ad0
Marc raphael <marcraphael979@gmail.com>
2023-01-25 01:40:09 +0100

    components/simple_book/Single_Book: Update removeBook handler

--- src/components/simple_book/Single_Book.js
+++ src/components/simple_book/Single_Book.js
@@ -1,16 +1,29 @@
-import RemoveBook from '../remove_a_book/Remove_a_Book';
+import PropTypes from 'prop-types';
 
-const SingleBook = (book) => {
-  const { title, author, id } = book;
+import useDispatch from 'react-redux';
+import { removeBook } from '../../redux/books/books';
+
+const SingleBook = (props) => {
+  const { title, author, id } = props;
+  const dispatch = useDispatch();
+  const RemoveBook = (id) => {
+    dispatch(removeBook(id));
+  };
   return (
     <div className="book">
       <div className="book-info">
         <h3 className="book-title">{title}</h3>
         <h4 className="book-author">{author}</h4>
       </div>
-      <button type="button" onClick={() => RemoveBook(id)}>Remove Book</button>
+      <button type="button" onClick={RemoveBook(id)}>Remove Book</button>
     </div>
   );
 };
 
 export default SingleBook;
+
+SingleBook.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+};
