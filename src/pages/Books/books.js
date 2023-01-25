a452f82a48c09db7e86e57547c2bf59e55461468
Marc raphael <marcraphael979@gmail.com>
2023-01-25 22:23:15 +0100

    pages/Books/books: update state consuming with useSelector

--- src/pages/Books/books.js
+++ src/pages/Books/books.js
@@ -1,19 +1,24 @@
+import React from 'react';
 import { useSelector } from 'react-redux';
 import SingleBook from '../../components/simple_book/Single_Book';
 import AddBook from '../../components/add_a_book/Add_a_Book';
 
 const Books = () => {
-  const books = useSelector((state) => state.book);
+  const books = useSelector((state) => state.booksReducer);
   return (
     <div>
       <ul className="books">
-        {books.map((book) => (
-          <SingleBook
-            key={book.id}
-            title={book.title}
-            author={book.author}
-          />
-        ))}
+        {!books.length
+          ? 'No books found'
+          : books.map((book) => (
+            <SingleBook
+              id={book.id}
+              key={book.id}
+              title={book.title}
+              author={book.author}
+              book={book}
+            />
+          ))}
       </ul>
       <AddBook />
 
