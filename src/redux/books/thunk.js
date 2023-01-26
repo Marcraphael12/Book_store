9ed743fc468db4850efeda3ce698505ec7a54a4b
Marc raphael <marcraphael979@gmail.com>
2023-01-26 10:31:58 +0100

    redux/books/thunk: Add post request for AddBook

--- src/redux/books/thunk.js
+++ src/redux/books/thunk.js
@@ -1,3 +1,17 @@
 import { getBook, addBook, removeBook } from './books';
 
+const ID = 'Nf6SeYddxW75X78pxU1n';
+const URL = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${ID}/books`;
 
+const AddABook = (book) => async (dispatch) => {
+  const result = await fetch(URL, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(book),
+  });
+  console.log(result);
+  dispatch(addBook(book));
+};
