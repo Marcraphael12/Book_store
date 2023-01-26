17855711b47b24fa908d4ba576849c9333032bc9
Marc raphael <marcraphael979@gmail.com>
2023-01-26 11:37:55 +0100

    pages/Books/books: Dispatch getBook

--- src/pages/Books/books.js
+++ src/pages/Books/books.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import SingleBook from '../../components/simple_book/Single_Book';
 import AddBook from '../../components/add_a_book/Add_a_Book';
+import { GetABook } from '../../redux/books/thunk';
 
 const Books = () => {
   const books = useSelector((state) => state.booksReducer);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(GetABook());
+  }, []);
   return (
     <div>
       <ul className="books">
@@ -12,8 +17,8 @@ const Books = () => {
           ? 'No books found'
           : books.map((book) => (
             <SingleBook
-              id={book.id}
-              key={book.id}
+              id={book.item_id}
+              key={book.item_id}
               title={book.title}
               author={book.author}
               book={book}
